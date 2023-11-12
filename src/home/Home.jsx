426877183fadc32ead9ee634e86361761c38c343
Sahin2002 <shahinderishov@iclound.com>
2023-11-12 20:57:50 +0300

    home add

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import './Home.css';
 import '../fonts/font.css'
-import background from'../image/trackmate.webp';
+import Trackmate from'../image/track.webp';
+import icon from'../image/section-2-icon.png';
+import smart from'../image/smart.png';
 import Navbar from "../navbar/Navbar";
 import Sebet from "../sebet/Sebet";
 function Home(){
@@ -17,19 +19,42 @@ function Home(){
       <div className="home">
         <Navbar seb={open} />
         <div className="section-1">
-          <div>
-            <h3>Günlük Rutininizi Yaratın</h3>
+          <div className="text">
+            <span>Günlük Rutininizi Yaratın</span>
             <h1>0.4 TrackMate XE</h1>
-            <h3>Her adımınızda sizi motive eden sağlık takip cihazı</h3>
+            <span>Her adımınızda sizi motive eden sağlık takip cihazı</span>
             <div className="button">
               <button>Hemen Al</button>
             </div>
           </div>
-          <div className="image">
-            <img src={background} alt="" />
+          <div className="imagess">
+            <img src={Trackmate} alt="" />
           </div>
         </div>
-         {seb ? <Sebet seb={close}  />:null }
+        <div className="section-2">
+          <div className="image1">
+            <div className="text">
+              <div className="icons">
+                <img src={icon} alt="" />
+                <span>EGZERSİZ TAKİBİ</span>
+              </div>
+              <div>
+                <h1>Doğru Egzersiz İstatistikleriyle Rutininizi Ayarlayın</h1>
+                <p>
+                  Bu, bir paragraf. Kendi metninizi eklemek için tıklayın.
+                  İçeriğinizi eklemek ve yazı tipini değiştirmek için “Metni
+                  Düzenle”ye tek tıklayın veya buraya çift tıklayın.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="image2">
+            <div className="section2-image">
+              <img src={smart} alt="" />
+            </div>
+          </div>
+        </div>
+        {seb ? <Sebet seb={close} /> : null}
       </div>
     );
 }
--- src/sss/Sss.jsx
+++ src/sss/Sss.jsx
@@ -1,24 +1,197 @@
-import React,{useState} from "react";
-import './sss.css';
+import React, { useState } from "react";
+import "./sss.css";
 import Navbar from "../navbar/Navbar";
 import Sebet from "../sebet/Sebet";
-function Sss(){
-   const [seb, SetSeb] = useState(false);
-   const open = () => {
-     SetSeb(true);
-     console.log(seb);
-   };
-   const close = () => {
-     SetSeb(false);
-   };
-  
-    
-    return (
-      <div className="SSS">
-        <Navbar seb={open}/>
-        <div>SSS</div>
-        {seb ? <Sebet seb={close} /> : null}
+import Icon from'../image/img.webp';
+function Sss() {
+  const [gen, SetGen] = useState(false);
+  const [seb, SetSeb] = useState(false);
+  const open = () => {
+    SetSeb(true);
+    console.log(seb);
+  };
+  const close = () => {
+    SetSeb(false);
+  };
+  const opens = () => {
+    SetGen(true);
+    console.log(seb);
+  };
+  const closes = () => {
+    SetGen(false);
+  };
+
+  return (
+    <div className="SSS">
+      <Navbar seb={open} />
+      <div className="sss-menu">
+        <h1>SSS</h1>       
+        
+        <div className="list">
+          <ul className="menu">
+            <li onClick={closes}>SSS'lerin Ayarlanması</li>
+            <li onClick={opens}>Genel</li>
+          </ul>
+          <div className="list-menu">
+            {gen ? (
+              <div>
+                <div className="page-1">
+                  <div className="text">
+                    <h3>SSS Bölümü ne işe yarar?</h3>
+                    <div>
+                      <span></span>
+                    </div>
+                  </div>
+                  <div className="div">
+                    <p>
+                      SSS bölümü, "Gönderim bölgeleriniz nelerdir?", "Çalışma
+                      saatleriniz nelerdir?" veya "Nasıl hizmet randevusu
+                      alabilirim?" gibi, işletmenizle ilgili yaygın olarak
+                      sorulan sorulara cevap vermek için kullanılabilir.
+                      SSS'lar, ziyaretçilerinizin sitenizde kolayca gezinmesine
+                      yardım eder ve hatta sitenizin SEO'sunu geliştirebilir.
+                    </p>
+
+                    <img src={Icon} alt="" />
+                    <ul>
+                      <li>
+                        <i class="fa-brands fa-facebook"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-x-twitter"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-linkedin"></i>
+                      </li>
+                      <li>
+                        <i class="fa-solid fa-link"></i>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <div className="page-1">
+                  <div className="text">
+                    <h3>Yeni bir soru ve cevap nasıl eklenir?</h3>
+                    <div>
+                      <span></span>
+                    </div>
+                  </div>
+                  <div className="div">
+                    <p>Yeni bir SSS eklemek için şu adımları izleyin:</p>
+                    <ol>
+                      <li>SSS'ları Yönet' düğmesine tıklayın.</li>
+                      <li>
+                        Sitenizin kontrol panelinden 'Yeni Ekle' düğmesine
+                        tıklayın ve ardından 'Soru ve cevap' seçeneğini seçin.
+                      </li>
+                      <li>
+                        Yeni eklenen her soru ve cevap, bir kategoriye
+                        atanmalıdır.
+                      </li>
+                      <li>Kaydedin ve yayınlayın.</li>
+                    </ol>
+                    <p>
+                      İstediğiniz her zaman SSS'larınızı düzenleyebilir,
+                      sıralarını değiştirebilir ve başka kategoriler
+                      seçebilirsiniz.
+                    </p>
+                    <ul>
+                      <li>
+                        <i class="fa-brands fa-facebook"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-x-twitter"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-linkedin"></i>
+                      </li>
+                      <li>
+                        <i class="fa-solid fa-link"></i>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="page-1">
+                  <div className="text">
+                    <h3>
+                      SSS'larıma bir görüntü, video veya gif yerleştirebilir
+                      miyim?
+                    </h3>
+                    <div>
+                      <span></span>
+                    </div>
+                  </div>
+                  <div className="div">
+                    <p>Evet. Medya eklemek için şu adımları izleyin:</p>
+                    <ol>
+                      <li>Uygulamanın Ayarlar bölümüne girin</li>
+                      <li>'SSS'ları Yönet' düğmesine tıklayın</li>
+                      <li>
+                        Medya eklemek istediğiniz soruyu oluşturun veya mevcut
+                        sorular arasından seçin
+                      </li>
+                      <li>
+                        Cevabınızı düzenlerken video, görüntü veya gif'in
+                        üzerine tıklayın
+                      </li>
+                      <li>Kütüphanenizden medya ekleyin ve kaydedin</li>
+                    </ol>
+                    <ul>
+                      <li>
+                        <i class="fa-brands fa-facebook"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-x-twitter"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-linkedin"></i>
+                      </li>
+                      <li>
+                        <i class="fa-solid fa-link"></i>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="page-1">
+                  <div className="text">
+                    <h3>"SSS" başlığını nasıl düzenler veya kaldırırım?</h3>
+                    <div>
+                      <span></span>
+                    </div>
+                  </div>
+                  <div className="div">
+                    <p>
+                      Başlığı, uygulamadaki Ayarlar sekmesinden
+                      düzenleyebilirsiniz. Başlığı göstermek istemiyorsanız
+                      'Gösterilecek Bilgiler' altından başlığı devre dışı
+                      bırakın.
+                    </p>
+                    <ul>
+                      <li>
+                        <i class="fa-brands fa-facebook"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-x-twitter"></i>
+                      </li>
+                      <li>
+                        <i class="fa-brands fa-linkedin"></i>
+                      </li>
+                      <li>
+                        <i class="fa-solid fa-link"></i>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    );
+      {seb ? <Sebet seb={close} /> : null}
+    </div>
+  );
 }
 export default Sss;
