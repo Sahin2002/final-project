c42638f5c2a72037f3484f6dbc92032c618ecc3e
Sahin2002 <shahinderishov@iclound.com>
2023-11-18 18:57:18 +0300

    sepet

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './fonts/font.css';
 import Home from'./home/Home';
 import Sss from'./sss/Sss';
 import Login from './login/Login';
-import Saat from'./smartsaat/smartsaat';
+import Saat from'./smartsaat/Smartsaat';
 import{Routes,Route} from'react-router-dom';
 function App() {
   return (
--- src/smartsaat/smartsaat.jsx
+++ src/smartsaat/smartsaat.jsx
@@ -2,42 +2,97 @@ import React from "react";
 import "./smartsaat.css";
 import Navbar from "../navbar/Navbar";
 import Footer from "../footer/footer";
-import white from "../image/white.webp";
-
-function smartsaat() {
+import beyaz from "../image/white.webp";
+import siyah from'../image/black.webp';
+import mavi from'../image/blue.webp';
+import mor from'../image/mor.webp';
+import turkaz from "../image/mavi.webp";
+import { useState } from "react";
+function Smartsaat() {
+  const [sent , setSent]=useState(850.00);
+  const [img,setImg]=useState(beyaz);
+  const [color,setColor]=useState("beyaz");
+  const  dey=(event)=>{
+    console.log(event.target.value*sent)
+  }
+  const urun=(event)=>{
+    setColor(event.target.className)
+    setImg(event.target.id)
+  }
+  
   return (
     <div className="urun">
       <Navbar />
       <div className="divs">
-        <div>
-          <img src={white} alt="" />
+        <div className="left">
+          <img src={img} alt="" />
           <p>
             Bu, bir ürün açıklaması. Burası, ürününüz ile ilgili boyut, malzeme,
             bakım ve temizlik talimatları gibi daha fazla ayrıntı eklemek için
             harika bir yer.
           </p>
         </div>
-        <div>
+        <div className="right">
           <h1>0.4 TrackMate XE</h1>
           <span>Ürün Kodu: 0001</span>
-          <h3>850,00₺</h3>
+          <h3>{sent}₺</h3>
           <div className="colors">
-            <span>reng:</span>
+            <span>reng:{color}</span>
             <div className="color">
-
+              <div onClick={urun} className="beyaz" id={beyaz}></div>
+              <div onClick={urun} className="siyah" id={siyah}></div>
+              <div onClick={urun} className="mavi" id={mavi}></div>
+              <div onClick={urun} className="mor" id={mor}> </div>
+              <div onClick={urun} className="turkaz" id={turkaz}></div>
             </div>
             <div className="number">
-                <span>Adet</span>
-                <input type="number" name="" id="" />
-                <div className="button">
-                    <button>Sebet ekle</button>
-                </div>
+              <span>Adet</span>
+              <input onClick={dey} type="number" min={1} name="" id="" />
+              <div className="button">
+                <button>Sebet ekle</button>
+              </div>
             </div>
           </div>
+          <div className="about">
+            <div>
+              <span>Urun Bilgisi</span>
+              <div>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Minus quis odio perferendis error! Accusantium necessitatibus
+                  corrupti tempore expedita, nihil voluptatem, natus fugiat,
+                  possimus minus non dolorem cupiditate error earum odit.
+                </p>
+              </div>
+            </div>
+            <div>
+              <span>Urun Bilgisi</span>
+              <div>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Minus quis odio perferendis error! Accusantium necessitatibus
+                  corrupti tempore expedita, nihil voluptatem, natus fugiat,
+                  possimus minus non dolorem cupiditate error earum odit.
+                </p>
+              </div>
+            </div>
+            <div>
+              <span>Urun Bilgisi</span>
+              <div>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Minus quis odio perferendis error! Accusantium necessitatibus
+                  corrupti tempore expedita, nihil voluptatem, natus fugiat,
+                  possimus minus non dolorem cupiditate error earum odit.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="sosyal-media"></div>
         </div>
       </div>
       <Footer />
     </div>
   );
 }
-export default smartsaat;
+export default Smartsaat;
